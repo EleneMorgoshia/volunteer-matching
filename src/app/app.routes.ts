f1f774a67cef5081a9609077a3fe19561d6a60af
EleneMorgoshia <elenemorgoshia.20049@gmail.com>
2026-02-03 22:32:36 +0700

    created login page component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { LOGIN_ROUTES } from './features/auth-entry/components/login/login.routes';
 
-export const routes: Routes = [];
+export const routes: Routes = [...LOGIN_ROUTES];
