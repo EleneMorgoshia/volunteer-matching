7cc6c04d10836f0a9994e18df8beb951915f8f79
EleneMorgoshia <elenemorgoshia.20049@gmail.com>
2026-02-14 14:46:44 +0700

    added footer component

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './shared/ui/header/header';
+import { Footer } from './shared/ui/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
