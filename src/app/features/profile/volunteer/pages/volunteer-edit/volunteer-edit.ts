91f4d5b50bd372f6cb26df3fed62371849557981
EleneMorgoshia <elenemorgoshia.20049@gmail.com>
2026-04-08 22:34:38 +0700

    added boilerplate for profiles

---
+++ src/app/features/profile/volunteer/pages/volunteer-edit/volunteer-edit.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-volunteer-edit',
+  imports: [],
+  templateUrl: './volunteer-edit.html',
+  styleUrl: './volunteer-edit.scss',
+})
+export class VolunteerEdit {
+
+}
---
+++ src/app/features/profile/volunteer/pages/voluteer-profile/voluteer-profile.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-voluteer-profile',
+  imports: [RouterOutlet],
+  templateUrl: './voluteer-profile.html',
+  styleUrl: './voluteer-profile.scss',
+})
+export class VoluteerProfile {}
